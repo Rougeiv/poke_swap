d909ac77d9c62a6a7639496f7e1da6d557c992f7
ricccooo <enricotionandra@gmail.com>
2024-04-30 14:59:41 +0800

    Removed naviationbar.html, footer.html and a duplicate of index.html. Combined the functionality of navigationbar.html and footer.html into base.html. Hence making base.html the main template and index.html only a child template. Modified app.py to suit changes.

--- src/pages/app.py
+++ src/pages/app.py
@@ -6,13 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/navigationbar')
-def navigation_bar():
-    return render_template('navigationbar.html')
-
-@app.route('/footer')
-def footer():
-    return render_template('footer.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
