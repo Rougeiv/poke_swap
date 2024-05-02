2db89ffd42278b4c2e8f10af3117c61a49bab18e
AarifLamat <aarif.lamat@outlook.com>
2024-05-02 11:37:24 +0800

    moved  trade below pokemon table, changed trade to reference pokemonID rather than name and migrated + upgraded db

--- app/models.py
+++ app/models.py
@@ -13,11 +13,11 @@ class User(db.Model):
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     
-    user_id1_trades: so.Mapped[List['Trade']] = so.relationship(
-        'Trade', foreign_keys="[Trade.user_id1]", back_populates='user', lazy=True)
+    user_id1_trades: so.Mapped['Trade'] = so.relationship(
+        'Trade', foreign_keys="[Trade.user_id1]", back_populates='user1', lazy=True)
     
-    user_id2_trades: so.Mapped[List['Trade']] = so.relationship(
-        'Trade', foreign_keys="[Trade.user_id2]", back_populates='user', lazy=True)
+    user_id2_trades: so.Mapped['Trade'] = so.relationship(
+        'Trade', foreign_keys="[Trade.user_id2]", back_populates='user2', lazy=True)
 
     inventory = so.relationship(
         'Pokemon', secondary='user_pokemon', back_populates='owners', lazy=True)
@@ -32,24 +32,6 @@ class User(db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
-
-class Trade(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    timestamp: so.Mapped[datetime] = so.mapped_column(
-        index=True, default=lambda: datetime.now(timezone.utc))
-    pokemon1: so.Mapped[str] = so.mapped_column(sa.String(32))
-    pokemon2: so.Mapped[str] = so.mapped_column(sa.String(32))
-    
-    # Define user_id1 and user_id2 as foreign keys
-    user_id1: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'), index=True)
-    user_id2: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'), index=True)
-
-    # Define the relationship with User for user_id1 and user_id2
-    user1 = so.relationship('User', foreign_keys=[user_id1], back_populates='user_id1_trades')
-    user2 = so.relationship('User', foreign_keys=[user_id2], back_populates='user_id2_trades')
-    
-    def __repr__(self):
-        return '<Trade: {} gave {} <-> {} given by {}>'.format(self.user_id1, self.pokemon1, self.pokemon2, self.user_id2)
 
 class Pokemon(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -66,3 +48,35 @@ user_pokemon: so.Mapped[sa.Table] = db.Table('user_pokemon',
     sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'), primary_key=True),
     sa.Column('pokemon_id', sa.Integer, sa.ForeignKey('pokemon.id'), primary_key=True)
 )
+
+class Trade(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    
+    # Define pokemon_id1 and pokemon_id2 as foreign keys
+    pokemon_id1: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Pokemon.id, name='fk_trade_pokemon_id1'), index=True)
+    pokemon_id2: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Pokemon.id, name='fk_trade_pokemon_id2'), index=True)
+    
+    # Define the relationship with Pokemon for pokemon_id1 and pokemon_id2
+    pokemon1 = so.relationship('Pokemon', foreign_keys=[pokemon_id1])
+    pokemon2 = so.relationship('Pokemon', foreign_keys=[pokemon_id2])
+
+    # Define user_id1 and user_id2 as foreign keys
+    user_id1: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id, name='fk_trade_user_id1'), index=True)
+    user_id2: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id, name='fk_trade_user_id2'), index=True)
+
+    # Define the relationship with User for user_id1 and user_id2
+    user1 = so.relationship('User', foreign_keys=[user_id1], back_populates='user_id1_trades')
+    user2 = so.relationship('User', foreign_keys=[user_id2], back_populates='user_id2_trades')
+
+    def __repr__(self):
+        # Get usernames of the users involved in the trade
+        user1_name = self.user1.username if self.user1 else 'Unknown User'
+        user2_name = self.user2.username if self.user2 else 'Unknown User'
+
+        # Get names of the Pokemon involved in the trade
+        pokemon1_name = self.pokemon1.name if self.pokemon1 else 'Unknown Pokemon'
+        pokemon2_name = self.pokemon2.name if self.pokemon2 else 'Unknown Pokemon'
+
+        return '<Trade: {user1_name} traded {pokemon1_name} <-> {user2_name} traded {pokemon2_name}>'
