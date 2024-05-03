82314fbcbb0d61fa6d6e555a9ec8fda59317fdac
ricccooo <enricotionandra@gmail.com>
2024-05-03 11:27:31 +0800

    Removed hard coding links. Added login and logout button. Adjusted app.py to accept {{url_for}} format.

--- app/app.py
+++ app/app.py
@@ -6,5 +6,33 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/how_to_play')
+def how_to_play():
+    return render_template('howtoplay.html')
+
+@app.route('/catch')
+def catch():
+    return render_template('catch.html')
+
+@app.route('/trade_request')
+def trade_request():
+    return render_template('traderequest.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('logout.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/trade_offer')
+def trade_offer():
+    return render_template('tradeoffer.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
